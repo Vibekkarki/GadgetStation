92aa0c251f11003589d2b1bdfc60dec72d5ed0b0
Vibekkarki <58102435+Vibekkarki@users.noreply.github.com>
2024-12-12 22:55:53 -0500

    Add project files.

---
+++ GadgetStation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GadgetStation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
